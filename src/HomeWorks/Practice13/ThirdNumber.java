b1bc8b82694ffc42cfe0270a2531264c32f19f3d
SampliX <gtntg0987654321>
2020-10-23 15:38:41 +0300

    Full practice 13
    Signed-off-by: SampliX <gtntg0987654321>

---
+++ src/HomeWorks/Practice13/ThirdNumber.java
@@ -0,0 +1,47 @@
+package HomeWorks.Practice13;
+
+import java.util.Scanner;
+
+public class ThirdNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        String line = read.nextLine();
+        String aLine = "";
+        String endLine = "";
+        int it = 0;
+        boolean check = false;
+
+        for(int i = 0; i < line.length();i++)
+        {
+            if(line.charAt(i) > 47 && line.charAt(i) < 58)
+            {
+                it++;
+                aLine += line.charAt(i);
+            }
+        }
+
+        if(it == 10)
+        {
+            endLine = "+" + 7 +
+                    " (" + aLine.charAt(0) + aLine.charAt(1) + aLine.charAt(2) + ") " +
+                    aLine.charAt(3) + aLine.charAt(4) + aLine.charAt(5) + "-" +
+                    aLine.charAt(6) + aLine.charAt(7) + "-" +
+                    aLine.charAt(8) + aLine.charAt(9);
+            System.out.println(endLine);
+        }
+        else if (it == 11 && (aLine.charAt(0) == '7' || aLine.charAt(0) == '8'))
+        {
+            endLine = "+" + 7 +
+                    " (" + aLine.charAt(1) + aLine.charAt(2) + aLine.charAt(3) + ") " +
+                    aLine.charAt(4) + aLine.charAt(5) + aLine.charAt(6) + "-" +
+                    aLine.charAt(7) + aLine.charAt(8) + "-" +
+                    aLine.charAt(9) + aLine.charAt(10);
+            System.out.println(endLine);
+        }
+        else
+            System.out.println("Некорректная входная строка");
+
+    }
+}
