db65253858df3508724877ee795e17af58e9d00e
SampliX <gtntg0987654321>
2020-10-24 13:43:49 +0300

    Practice 13 №3 fix
    Signed-off-by: SampliX <gtntg0987654321>

--- src/HomeWorks/Practice13/ThirdNumber.java
+++ src/HomeWorks/Practice13/ThirdNumber.java
@@ -1,5 +1,4 @@
 package HomeWorks.Practice13;
-
 import java.util.Scanner;
 
 public class ThirdNumber
@@ -9,41 +8,21 @@ public class ThirdNumber
         Scanner read = new Scanner(System.in);
         String line = read.nextLine();
         String aLine = "";
-        String endLine = "";
         int it = 0;
         boolean check = false;
 
-        for(int i = 0; i < line.length();i++)
-        {
-            if(line.charAt(i) > 47 && line.charAt(i) < 58)
-            {
+        for(int i = 0; i < line.length();i++) {
+            if (line.charAt(i) > 47 && line.charAt(i) < 58) {
                 it++;
                 aLine += line.charAt(i);
-            }
-            else if(line.charAt(i) != '-' || line.charAt(i) != '(' || line.charAt(i) != ')' || line.charAt(i) != '-')
+            } else if (line.charAt(i) != '-' || line.charAt(i) != '(' || line.charAt(i) != ')' || line.charAt(i) != '+')
                 check = true;
         }
-
         if(it == 10 && !check)
-        {
-            endLine = "+" + 7 +
-                    " (" + aLine.charAt(0) + aLine.charAt(1) + aLine.charAt(2) + ") " +
-                    aLine.charAt(3) + aLine.charAt(4) + aLine.charAt(5) + "-" +
-                    aLine.charAt(6) + aLine.charAt(7) + "-" +
-                    aLine.charAt(8) + aLine.charAt(9);
-            System.out.println(endLine);
-        }
+            System.out.println("+" + 7 + " (" + aLine.substring(0,3) + ") " + aLine.substring(3,6) + "-" + aLine.substring(6,8) + "-" + aLine.substring(8,10));
         else if (it == 11 && (aLine.charAt(0) == '7' || aLine.charAt(0) == '8') && !check)
-        {
-            endLine = "+" + 7 +
-                    " (" + aLine.charAt(1) + aLine.charAt(2) + aLine.charAt(3) + ") " +
-                    aLine.charAt(4) + aLine.charAt(5) + aLine.charAt(6) + "-" +
-                    aLine.charAt(7) + aLine.charAt(8) + "-" +
-                    aLine.charAt(9) + aLine.charAt(10);
-            System.out.println(endLine);
-        }
+            System.out.println("+" + 7 + " (" + aLine.substring(1,4) + ") " + aLine.substring(4,7) + "-" + aLine.substring(7,9) + "-" + aLine.substring(9,11));
         else
             System.out.println("Некорректная входная строка");
-
     }
 }
