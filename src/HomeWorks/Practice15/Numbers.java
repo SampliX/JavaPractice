a49f319f363d67af5233a9f3a41bc798a4dee4ec
SampliX <petrov.pavel02@yandex.ru>
2020-11-05 22:54:31 +0300

    Practice15 All

--- src/HomeWorks/Practice15/Numbers.java
+++ src/HomeWorks/Practice15/Numbers.java
@@ -1,18 +1,23 @@
 package HomeWorks.Practice15;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class Numbers
 {
     static char[] letters = {'А','В','Е','К','М','Н','О','Р','С','Т','У','Х'};
     static ArrayList<String> numbers = new ArrayList<String>();
+    static Scanner read = new Scanner(System.in);
     static String str;
-    static Random rnd = new Random();
+    static String line;
+    static int binarySearch;
+    static boolean have = false;
+    static long timeStart;
+    static long timeStop;
 
     public static void main(String[] args)
     {
         generateNumbers();
+        searchNumber();
     }
 
     static void generateNumbers()
@@ -37,5 +42,43 @@ public class Numbers
         }
 
         System.out.println(numbers.size());
+    }
+
+    static void searchNumber()
+    {
+        HashSet<String> numbersHash = new HashSet<String>(numbers);
+        TreeSet<String> numbersTree = new TreeSet<String>(numbers);
+
+        System.out.println("Введите номер: ");
+        line = read.nextLine();
+
+        timeStart = System.nanoTime();
+        have = numbers.contains(line);
+        timeStop = System.nanoTime();
+
+        str = have? ("Такой номер есть") : ("Такого номера нет");
+        System.out.println("Поиск перебором: " + str + ", поиск занял: " + String.valueOf(timeStop - timeStart) + " нс");
+
+        Collections.sort(numbers);
+        timeStart = System.nanoTime();
+        binarySearch = Collections.binarySearch(numbers,line);
+        timeStop = System.nanoTime();
+
+        str = binarySearch>=0? ("Такой номер есть") : ("Такого номера нет");
+        System.out.println("Бинарный  поиск: " + str + ", поиск занял: " + String.valueOf(timeStop - timeStart) + " нс");
+
+        timeStart = System.nanoTime();
+        have = numbersHash.contains(line);
+        timeStop = System.nanoTime();
+
+        str = have? ("Такой номер есть") : ("Такого номера нет");
+        System.out.println("Поиск в HashSet: " + str + ", поиск занял: " + String.valueOf(timeStop - timeStart) + " нс");
+
+        timeStart = System.nanoTime();
+        have = numbersTree.contains(line);
+        timeStop = System.nanoTime();
+
+        str = have? ("Такой номер есть") : ("Такого номера нет");
+        System.out.println("Поиск в TreeSet: " + str + ", поиск занял: " + String.valueOf(timeStop - timeStart) + " нс");
     }
 }
