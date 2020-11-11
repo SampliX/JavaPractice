cae597442872b715c73dd2d55fd2a4e29a9a685b
SampliX <petrov.pavel02@yandex.ru>
2020-11-11 23:27:15 +0300

    Practice16 All

--- src/HomeWorks/Practice16/WorkWithFiles.java
+++ src/HomeWorks/Practice16/WorkWithFiles.java
@@ -1,6 +1,10 @@
 package HomeWorks.Practice16;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Scanner;
 
 public class WorkWithFiles
 {
@@ -8,25 +12,41 @@ public class WorkWithFiles
     {
         int it = 0;
         String line = "";
-        File folder = new File("E:\\Steam");
-        double result = getFolderSize(folder);
+        String path;
+        Scanner read = new Scanner(System.in);
 
-        while(result > 1024) {
-            result /= 1024;
-            it++;
+        try
+        {
+            System.out.print("Введите путь до папки: ");
+            path = read.nextLine();
+            File folder = new File(path);
+            double result = getFolderSize(folder);
+
+            while (result > 1024)
+            {
+                result /= 1024;
+                it++;
+            }
+
+            if (it == 0)
+                line = " byte ";
+            else if (it == 1)
+                line = " Kb ";
+            else if (it == 2)
+                line = " Mb ";
+            else if (it >= 3)
+                line = " Gb ";
+
+            System.out.print("Её размер: ");
+            System.out.format("%.1f",result);
+            System.out.print(line);
+
+            appendLogs(new Date().toString() + " - " + "folder: " + path + ", size: " + result + line + "\r");
         }
-
-        if(it == 0)
-            line = "байт";
-        else if(it == 1)
-            line = "Кб";
-        else if(it == 2)
-            line = "Мб";
-        else if(it >= 3)
-            line = "Гб";
-
-        System.out.format("%.1f",result);
-        System.out.println(" " + line);
+        catch (Exception ex)
+        {
+            System.out.println(ex.toString());
+        }
     }
 
     public static double getFolderSize(File folder) {
@@ -43,5 +63,15 @@ public class WorkWithFiles
                 size += getFolderSize(files[i]);
         }
         return size;
+    }
+
+    public static void appendLogs(String input) throws IOException
+    {
+        String logPath = "Logs\\log.txt";
+
+        FileOutputStream outputStream = new FileOutputStream(logPath, true);
+
+        outputStream.write(input.getBytes());
+        outputStream.close();
     }
 }
---
+++ src/HomeWorks/Practice16/WorkWithFiles1.java
@@ -0,0 +1,65 @@
+package HomeWorks.Practice16;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class WorkWithFiles1
+{
+    public static void copyFiles(File oldFolder, File newFolder) throws IOException
+    {
+        if(oldFolder.isFile())
+        {
+            System.out.println("file");
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try
+            {
+                inputStream = new FileInputStream(oldFolder);
+                outputStream = new FileOutputStream(newFolder);
+
+                byte[] buffer = new byte[1024];
+                int length;
+
+                while ((length = inputStream.read(buffer)) > 0)
+                    outputStream.write(buffer, 0, length);
+            }
+            finally
+            {
+                inputStream.close();
+                outputStream.close();
+            }
+        }
+        else if(oldFolder.isDirectory())
+        {
+            System.out.println("folder");
+            String[] children = oldFolder.list();
+            for (int i=0; i<children.length; i++)
+                copyFiles(new File(oldFolder, children[i]), new File(newFolder, children[i]));
+        }
+        System.out.println("gvjh");
+
+        if(!oldFolder.exists())
+        {
+            System.out.println("Начальная директория не существует");
+        }
+
+        if(!newFolder.exists())
+        {
+            System.out.println("Конечная директория не существует");
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Введите начальную директорию");
+        String oldFolder = read.nextLine();
+
+        System.out.println("Введите конечную директорию");
+        String newFolder = read.nextLine();
+
+        copyFiles(new File(oldFolder),new File(newFolder));
+    }
+}
