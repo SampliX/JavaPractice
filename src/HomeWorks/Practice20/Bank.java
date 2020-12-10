430ebeea5148365b10a27647ef4bc2aba8cbd02c
SampliX <petrov.pavel02@yandex.ru>
2020-12-11 00:14:50 +0300

    Practice20

--- src/HomeWorks/Practice20/Bank.java
+++ src/HomeWorks/Practice20/Bank.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
+import java.lang.Thread;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Bank implements Runnable
 {
@@ -14,8 +16,14 @@ public class Bank implements Runnable
     static private final Random random = new Random();
     static String line = "";
     static Scanner read = new Scanner(System.in);
+    static String aa = "";
+    static String a = "", b = "";
+    static long c = 0;
+    static AtomicBoolean balance = new AtomicBoolean();
+    static Bank myThread = new Bank();
+    private static Thread bankMulti = new Thread(myThread,"bank");
 
-    public static void main(String[] args) throws InterruptedException
+    public static void main(String[] args)
     {
         accounts.put("Генадий", new Account(47826, "Генадий"));
         accounts.put("Федор", new Account(24655, "Федор"));
@@ -26,11 +34,16 @@ public class Bank implements Runnable
         accounts.put("Дмитрий", new Account(65752, "Дмитрий"));
         accounts.put("Ринат", new Account(12678, "Ринат"));
 
-        for(int i = 0; i < accounts.size(); i++)
-            bankMoney += accounts.get(i).money;
+        ArrayList<Account> values = new ArrayList<>(accounts.values());
 
+        for(int i = 0; i < accounts.size(); i++)
+            bankMoney += values.get(i).money;
+
+        System.out.println("Терминал банка. Вам доступны две команды: баланс {имя клиента банка}, перевод {имя клиента, у которого хотите снять деньги} {имя клиента, которому хотите передать сумму} {сумма перевода}");
 
         line = read.nextLine();
+
+        balance.set(false);
 
         while (!line.equals("выход"))
         {
@@ -38,16 +51,28 @@ public class Bank implements Runnable
 
             switch (lineArr[0]) {
                 case ("баланс"):
-                    if (lineArr.length == 2)
-                        System.out.println(getBalance(lineArr[1]));
+                    if (lineArr.length == 2 && accounts.containsKey(lineArr[1]))
+                    {
+                        balance.set(true);
+                        aa = lineArr[1];
+                        c = 0;
+                        bankMulti = new Thread(myThread,"bank");
+                        bankMulti.start();
+                    }
                     else
-                        System.out.println("Неизвестная команда");
+                        System.out.println("Данных о пользователе не существует");
                     break;
                 case ("перевод"):
-                    if (lineArr.length == 4)
-                        transfer(lineArr[1],lineArr[2],Long.parseLong(lineArr[3]));
+                    if (lineArr.length == 4 && accounts.containsKey(lineArr[1]) && accounts.containsKey(lineArr[2]))
+                    {
+                        a = lineArr[1];
+                        b = lineArr[2];
+                        c = Long.parseLong(lineArr[3]);
+                        bankMulti = new Thread(myThread,"bank");
+                        bankMulti.start();
+                    }
                     else
-                        System.out.println("Неизвестная команда");
+                        System.out.println("Данные о пользователях не существуют");
                     break;
                 default:
                     System.out.println("Неизвестная команда");
@@ -57,13 +82,16 @@ public class Bank implements Runnable
         }
     }
 
-    public synchronized static boolean isFraud(String fromAccountNum, String toAccountNum, long amount) throws InterruptedException
+    public static boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
     {
-        Thread.sleep(1000);
+        try {
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException e) { }
         return random.nextBoolean();
     }
 
-    public static synchronized void transfer(String fromAccountNum, String toAccountNum, long amount) throws InterruptedException
+    public static synchronized void transfer(String fromAccountNum, String toAccountNum, long amount)
     {
         if (!(blockedAccounts.contains(fromAccountNum) || blockedAccounts.contains(toAccountNum)))
         {
@@ -71,49 +99,69 @@ public class Bank implements Runnable
             {
                 if (amount <= 50000)
                 {
+                    checkAllSum();
                     accounts.get(fromAccountNum).money -= amount;
                     accounts.get(toAccountNum).money += amount;
-                    System.out.println("Перевод проведен успешно");
+                    System.out.println("Перевод " + fromAccountNum + " -> " + toAccountNum + " = " + amount + " проведен успешно");
+                    checkAllSum();
                 }
                 else
+                {
                     if (isFraud(fromAccountNum, toAccountNum, amount))
                     {
                         blockedAccounts.add(fromAccountNum);
                         blockedAccounts.add(toAccountNum);
-                        System.out.println("Перевод заблокирован. Подозрение в мошенничестве");
+                        System.out.println("Перевод " + fromAccountNum + " -> " + toAccountNum + " = " + amount + " заблокирован. Подозрение в мошенничестве");
                     }
                     else
                     {
+                        checkAllSum();
                         accounts.get(fromAccountNum).money -= amount;
                         accounts.get(toAccountNum).money += amount;
-                        System.out.println("Перевод проведен успешно");
+                        System.out.println("Перевод " + fromAccountNum + " -> " + toAccountNum + " = " + amount + " проведен успешно");
+                        checkAllSum();
                     }
+                }
             }
             else
             {
-                System.out.println("Перевод заблокирован. Недостаточно средств");
+                System.out.println("Перевод " + fromAccountNum + " -> " + toAccountNum + " = " + amount + " заблокирован. Недостаточно средств");
             }
         }
         else
         {
-            System.out.println("Перевод заблокирован. Служба Безопасности не позволила провести перевод");
+            System.out.println("Перевод " + fromAccountNum + " -> " + toAccountNum + " = " + amount + " заблокирован. Служба Безопасности не позволила провести перевод");
         }
     }
 
-    public static long getBalance(String accountNum)
+    public static void getBalance(String accountNum)
     {
-        return accounts.get(accountNum).money;
+        balance.set(false);
+        System.out.println("Пользователь: " + accountNum + "\n" + "Баланс: " + accounts.get(accountNum).money + " р");
     }
 
-    public void checkAllSum()
+    public static void checkAllSum()
     {
+        bankMoneyCheck = 0;
+
+        ArrayList<Account> values = new ArrayList<>(accounts.values());
+
         for(int i = 0; i < accounts.size(); i++)
-            bankMoneyCheck += accounts.get(i).money;
+            bankMoneyCheck += values.get(i).money;
+
+        if(bankMoney != bankMoneyCheck)
+        {
+            System.out.println("Произошла непридвиденная ошибка, повторите попытку снова");
+            System.exit(-1);
+        }
     }
 
     @Override
     public void run()
     {
-
+        if(!balance.get())
+            transfer(a,b,c);
+        else
+            getBalance(aa);
     }
 }
